13e921115c33a80a084ca4bb2be43a0ffc63a6d0
Andikarya17 <andikaryaputra17@gmail.com>
2024-06-04 14:39:38 +0700

    Menambahkan fungsionalitas untuk menulis input pengguna ke file sampai 'q' dimasukkan

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,4 +4,9 @@
 using namespace std;
 
 int main() {
-  
+    string baris;
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+
+    cout <<">=menulis file, \'q'\" untuk keluar << endl;
+}
